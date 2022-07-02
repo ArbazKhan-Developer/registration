1fc89a41abfeac71ac01e9a70d91be4df9078e14
ArbazKhan-Developer <kkabz860156@gmail.com>
2022-07-02 19:36:58 +0530

    return db response

--- api-operations/registration-service/src/api/RegistrationApiProcessor.js
+++ api-operations/registration-service/src/api/RegistrationApiProcessor.js
@@ -19,7 +19,7 @@ class RegistrationApiProcessor {
 
                 let dbResponse = await stageservice.processToDestination(requestBo.toJson())
 
-                resolve('api called successfully')
+                resolve(dbResponse)
             } catch (error) {
                 console.log(`error occoured in apiLayer:: ${error}`);
                 reject('error in api layer');
