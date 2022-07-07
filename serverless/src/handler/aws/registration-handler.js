b62e8774d90947e0b27f14b81de1115d7415b800
ArbazKhan-Developer <kkabz860156@gmail.com>
2022-07-07 12:18:19 +0530

    CORS enabled

--- serverless/src/handler/aws/registration-handler.js
+++ serverless/src/handler/aws/registration-handler.js
@@ -4,7 +4,21 @@ const apiProcessor = require("../../../api-operations/registration-service/src/a
 
 module.exports.registrationHandler = async (event, context, callback) => {
   try {
-   return await new apiProcessor().process(event, context);
+   return await new apiProcessor().process(event, context)
+    .then(result =>{
+      console.log(result);
+      callback(null, {
+        statusCode: 200,
+        "headers":{
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Headers": "content-Type, X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Code,Code-Varfier,applicationid",
+          "Access-Control-Allow-Methods": "OPTIONS,POST",
+          "Access-Control-Allow-Credentials": false,
+          "Access-Control-Allow-Origin": "*",
+          "X-Requested-With": "*"
+        }
+      })
+    })
   } catch (error) {
     console.log(error);
   }
